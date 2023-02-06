3ae0e89f4411bea3dc3cfa2458a9448e37c48abf
Herry Mandala <herrymandala@linuxmail.org>
2023-02-07 04:44:37 +0700

    Combine template with reactjs

---
+++ resources/views/app.blade.php
@@ -0,0 +1,25 @@
+
+<!doctype html>
+<html lang="en-US">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Interpretasi ID &#8211; Website baca berita dan artikel.</title>
+    <noscript>
+        <style>
+            .woocommerce-product-gallery {
+                opacity: 1 !important;
+            }
+        </style>
+    </noscript>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body class="home page-template page-template-elementor-template page-template-elementor-template-php page page-id-12 theme-benqu woocommerce-no-js ehf-template-benqu ehf-stylesheet-benqu elementor-default elementor-kit-5 elementor-page elementor-page-12">
+    <div id="app" class="pfy-main-wrapper"></div>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+
+</html>
