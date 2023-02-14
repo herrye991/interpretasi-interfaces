f9410f2e54c7eb31f9944f6eb3ee4044c3a4d325
Herry Mandala <herrymandala@linuxmail.org>
2023-02-14 17:20:46 +0700

    React : FirstSectionContentLeft.js [DONE]

--- resources/components/contents/FirstSectionContentLeft.js
+++ resources/components/contents/FirstSectionContentLeft.js
@@ -6,30 +6,31 @@ import Moment from 'moment';
 import Category from "../helpers/Category";
 
 export default function FirstSectionContentLeft() {
-    let [categoryData, setCategoryData] = React.useState([])
     let [firstData, setFirstData] = React.useState([])
     let [firstDataUser, setFirstDataUser] = React.useState([])
     let [secondData, setSecondData] = React.useState([])
     let [secondDataUser, setSecondDataUser] = React.useState([])
-    let [thirdData, setThirdData] = React.useState([])
-    let [thirdDataUser, setThirdDataUser] = React.useState([])
     useEffect(() => {
-        async function fetchData() { 
-            const requestOptions = {
-                headers: { 'Accept': 'application/json' },
-            };
-            axios.get(Env.apiURL('article/?tending=1&limit=3'), requestOptions).then((response) => {
+        const requestOptions = {
+            headers: { 'Accept': 'application/json' },
+        };
+        async function firstData() { 
+            axios.get(Env.apiURL('article/?tending=1&skip=0&take=1'), requestOptions).then((response) => {
                 setFirstData(response.data.data[0])
                 setFirstDataUser(response.data.data[0].user)
-                setSecondData(response.data.data[1])
-                setSecondDataUser(response.data.data[1].user)
-                setThirdData(response.data.data[2])
-                setThirdDataUser(response.data.data[2].user)
             }).catch((error) => {
                 console.log(error)
             });
         }
-        fetchData();
+        async function secondData() { 
+            axios.get(Env.apiURL('article/?tending=1&skip=1&take=2'), requestOptions).then((response) => {
+                setSecondData(response.data.data)
+            }).catch((error) => {
+                console.log(error)
+            });
+        }
+        firstData();
+        secondData();
     }, []);
     return (
         <div className="elementor-widget-wrap elementor-element-populated">
@@ -38,12 +39,10 @@ export default function FirstSectionContentLeft() {
                     <div className="pfy-post-grid-wrap pfy-overlay-style-9">
                         <div className="pfy-post-item pfy-scale">
                             <div className="pfy-post-thumb pfy-img-commn-style">
-                                <a title="Fasion Trends and Li Edelkoort the Culture Shock Special Report" href="#"><img loading="lazy" width="1500" height="1000" src={firstData.image} className="attachment-full size-full wp-post-image" alt="" decoding="async" /></a>
+                                <a title={ firstData.title } href={ Env.baseURL('article/' + firstData.url) }><img loading="lazy" width="1500" height="1000" src={firstData.image} className="attachment-full size-full wp-post-image" alt="" decoding="async" /></a>
                             </div>
                             <div className="pfy-post-content">
-                                <a className="benqu-cate-badge" href="#">
-                                    { Category(firstData.category_id).map((res, index) => { return <span key={ index }>{ res.name }</span> }) }
-                                </a>
+                                { Category(firstData.category_id).map((cat, index) => { return <a key={ index } className="benqu-cate-badge" style={ cat.color } href={ Env.apiURL('category/' + cat.id) }><span>{ cat.name }</span></a> }) }
                                 <h1 className="pfy-post-title">
                                     <a href={ Env.baseURL('article/' + firstData.url) }>{firstData.title}</a>
                                 </h1>
@@ -65,62 +64,24 @@ export default function FirstSectionContentLeft() {
 
                     <div className="pfy-post-grid-wrap grid-style-2 grid-style-9">
                         <div className="row">
-                            <div className="col-lg-6 col-md-6 col-xl-6">
+                            { secondData.map((res, index) =>
+                            <div className="col-lg-6 col-md-6 col-xl-6" key={index}>
                                 <div className="pfy-post-item pfy-scale pfy-ch">
                                     <div className="pfy-post-thumb pfy-img-commn-style">
-                                        <a className="post-thumb"
-                                            href="trip-to-iqaluit-in-nunavut-a-canadian-arctic-nice-city/index.html"><img
-                                                width="1500" height="1000"
-                                                src="https://interpretasi.id/assets/images/articles/default.png"
-                                                className="attachment-full size-full wp-post-image"
-                                                alt="" decoding="async" loading="lazy"
-                                                srcSet="https://interpretasi.id/assets/images/articles/default.png 1500w, https://interpretasi.id/assets/images/articles/default.png 300w, https://interpretasi.id/assets/images/articles/default.png 1024w, https://interpretasi.id/assets/images/articles/default.png 768w"
-                                                sizes="(max-width: 1500px) 100vw, 1500px" /></a>
-                                        <a className="benqu-cate-badge" href="category/travel/index.html">
-                                            <span>Travel</span>
-                                        </a>
+                                        <a className="post-thumb" href={ Env.baseURL('article/' + res.url) }><img width="1500" height="1000" src="https://interpretasi.id/assets/images/articles/default.png" className="attachment-full size-full wp-post-image" alt="" decoding="async" loading="lazy" /></a>
+                                        { Category(res.category_id).map((cat, index) => { return <a key={ index } className="benqu-cate-badge" style={ cat.color } href={ Env.baseURL("category/" + cat.id ) }><span>{ cat.name }</span></a> }) }
                                     </div>
                                     <div className="pfy-post-content">
-
-                                        <h3 className="pfy-post-title"><a
-                                            href="trip-to-iqaluit-in-nunavut-a-canadian-arctic-nice-city/index.html">Trip
-                                            To Iqaluit In Nunavut A Canadian Arctic Nice
-                                            City..</a></h3>
-
+                                        <h3 className="pfy-post-title"><a href={ Env.baseURL('article/' + res.url) }>{ res.title }</a></h3>
                                         <ul>
-                                            <li><span className="auth_by">BY</span> <a
-                                                href="author/bnque/index.html"
-                                                title="Posts by David" rel="author">David</a>
-                                            </li>
-
-                                            <li><i className="fal fa-calendar-alt"></i> Jul 07, 2022
-                                            </li>
-
-                                            <li><i className="far fa-comments"></i>
-                                                0 Comments </li>
-
-                                        </ul>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 col-md-6 col-xl-6">
-                                <div className="pfy-post-item pfy-scale pfy-ch">
-                                    <div className="pfy-post-thumb pfy-img-commn-style">
-                                        <a className="post-thumb"
-                                            href="smarter-food-choices-101-tips-for-the-busy-women/index.html"><img width="1500" height="1000" src="https://interpretasi.id/assets/images/articles/default.png" className="attachment-full size-full wp-post-image" alt="" decoding="async" loading="lazy" srcSet="https://interpretasi.id/assets/images/articles/default.png 1500w, https://interpretasi.id/assets/images/articles/default.png 300w, https://interpretasi.id/assets/images/articles/default.png 1024w, https://interpretasi.id/assets/images/articles/default.png 768w" sizes="(max-width: 1500px) 100vw, 1500px" /></a>
-                                        <a className="benqu-cate-badge" href="category/food/index.html"><span>Food</span></a>
-                                    </div>
-                                    <div className="pfy-post-content">
-                                        <h3 className="pfy-post-title"><a href="smarter-food-choices-101-tips-for-the-busy-women/index.html">Smarter Food Choices 101 Tips For The Busy Women</a></h3>
-                                        <ul>
-                                            <li><span className="auth_by">BY</span><a href="author/bnque/index.html" title="Posts by David" rel="author">David</a></li>
-                                            <li><i className="fal fa-calendar-alt"></i> Jul 07, 2022</li>
-                                            <li><i className="far fa-comments"></i> 0 Comments </li>
+                                            <li><span className="auth_by">Oleh &nbsp;</span><a href={ Env.baseURL('user/' + res.user.id) } title={"Diposting oleh " + res.user.name} rel="author"> {res.user.name }</a></li>
+                                            <li><i className="fal fa-calendar-alt"></i> {Moment(res.created_at).format('d MMM, YYYY')}</li>
+                                            <li><i className="far fa-comments"></i> { res.comments_count } Komentar </li>
                                         </ul>
                                     </div>
                                 </div>
                             </div>
+                            )}
                         </div>
                     </div>
                 </div>
--- resources/components/helpers/Category.js
+++ resources/components/helpers/Category.js
@@ -4,35 +4,51 @@ function Category (id) {
     const categories = [
         {
             "id": 1,
-            "name": "News"
+            "name": "News",
+            "color": { backgroundColor:"#00baff" },
+            "image": ""
         },
         {
             "id": 2,
-            "name": "Entertainment"
+            "name": "Entertainment",
+            "color": { backgroundColor:"#ffae25" },
+            "image": ""
         },
         {
             "id": 3,
-            "name": "Mom"
+            "name": "Mom",
+            "color": { backgroundColor:"#fc16a8" },
+            "image": ""
         },
         {
             "id": 4,
-            "name": "Food & Travel"
+            "name": "Food & Travel",
+            "color": { backgroundColor:"#ff4f00" },
+            "image": ""
         },
         {
             "id": 5,
-            "name": "Tekno & Sains"
+            "name": "Tekno & Sains",
+            "color": { backgroundColor:"#007bff" },
+            "image": ""
         },
         {
             "id": 6,
-            "name": "Otomotif"
+            "name": "Otomotif",
+            "color": { backgroundColor:"#00baff" },
+            "image": ""
         },
         {
             "id": 7,
-            "name": "Wanita"
+            "name": "Wanita",
+            "color": { backgroundColor:"#fc16a8" },
+            "image": ""
         },
         {
             "id": 8,
-            "name": "Lifestyle"
+            "name": "Lifestyle",
+            "color": { backgroundColor:"#52a815" },
+            "image": ""
         }
     ];
 
