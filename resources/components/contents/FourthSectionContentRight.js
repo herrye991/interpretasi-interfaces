63d70aa376b87e3aae62f27870c79fdd7611fc53
Herry Mandala <herrymandala@linuxmail.org>
2023-02-16 16:13:26 +0700

    Resolved #11 & #12

--- resources/components/contents/FourthSectionContentRight.js
+++ resources/components/contents/FourthSectionContentRight.js
@@ -1,92 +1,42 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from 'react';
+import axios from 'axios';
+import Env from "../helpers/Env";
+import Moment from 'moment';
+import Category from "../helpers/Category";
 
 export default function FourthSectionContentRight() {
+    let [responseData, setResponseData] = React.useState([]);
+    const requestOptions = {
+        headers: { 'Accept': 'application/json' },
+    };
+
+    useEffect(() => {
+        axios.get(Env.apiURL('article?orderBy=mostView&category=2&skip=4&take=5'), requestOptions).then((response) => {
+            setResponseData(response.data.data)
+        }).catch((error) => {
+            console.log(error)
+        });
+    }, []);
     return (
         <div className="elementor-widget-wrap elementor-element-populated">
             <div className="elementor-element elementor-element-46834cb elementor-widget elementor-widget-benqu-post-list" data-id="46834cb" data-element_type="widget" data-widget_type="benqu-post-list.default">
                 <div className="elementor-widget-container">
                     <div className="pfy-post-list-wrap list-style-1 list-style-6">
-                        <div className="pfy-post-item pfy-scale pfy-flex">
+                        {/* Start Map */}
+                        { responseData.map((res, index) =>
+                        <div key={ index } className="pfy-post-item pfy-scale pfy-flex">
                             <div className="pfy-post-thumb pfy-img-commn-style">
-                                <a className="pfy-postthumb" href="#trip-iqaluit-nunavut-a-canadian-arctic-city/"><img width="1500" height="1000" src="https://interpretasi.id/assets/images/articles/default.png" className="attachment-full size-full wp-post-image" alt="" decoding="async" loading="lazy" srcSet="https://interpretasi.id/assets/images/articles/default.png 1500w, https://interpretasi.id/assets/images/articles/default.png-300x200.jpg 300w, https://interpretasi.id/assets/images/articles/default.png-1024x683.jpg 1024w, https://interpretasi.id/assets/images/articles/default.png-768x512.jpg 768w" sizes="(max-width: 1500px) 100vw, 1500px" /></a>
+                                <a className="pfy-postthumb" href={Env.baseURL('article/' + res.url)}><img width="1500" height="1000" src={ res.image } className="attachment-full size-full wp-post-image" alt="" decoding="async" loading="lazy" sizes="(max-width: 1500px) 100vw, 1500px" /></a>
                             </div>
                             <div className="pfy-post-content">
-
-                                <a className="benqu-cate-badge" href="#category/fashion/" >
-                                    <span>Fashion</span>
-                                </a>
-
-                                <h4 className="pfy-post-title"><a href="#trip-iqaluit-nunavut-a-canadian-arctic-city/">Trip Iqaluit Nunavut A Canadian Arctic City</a></h4>
-
-                                <span className="pfy-post-date"><i className="fal fa-calendar-alt"></i> Jul 07, 2022</span>
-
+                                <h4 className="pfy-post-title"><a href={Env.baseURL('article/' + res.url)}>{res.title.length > 58 ? `${res.title.substring(0, 58)}...` : res.title}</a></h4>
+                                <span className="pfy-post-date"><i className="fal fa-calendar-alt"></i> {Moment(res.created_at).format('d MMM, YYYY')}</span>
                             </div>
                         </div>
-                        <div className="pfy-post-item pfy-scale pfy-flex">
-                            <div className="pfy-post-thumb pfy-img-commn-style">
-                                <a className="pfy-postthumb" href="#sony-laptops-are-still-part-of-the-sony-family/"><img width="1500" height="1200" src="https://interpretasi.id/assets/images/articles/default.png" className="attachment-full size-full wp-post-image" alt="" decoding="async" loading="lazy" srcSet="https://interpretasi.id/assets/images/articles/default.png 1500w, https://interpretasi.id/assets/images/articles/default.png 300w, https://interpretasi.id/assets/images/articles/default.png 1024w, https://interpretasi.id/assets/images/articles/default.png 768w" sizes="(max-width: 1500px) 100vw, 1500px" /></a>
-                            </div>
-                            <div className="pfy-post-content">
-
-                                <a className="benqu-cate-badge" href="#category/creative/" >
-                                    <span>Creative</span>
-                                </a>
-
-                                <h4 className="pfy-post-title"><a href="#sony-laptops-are-still-part-of-the-sony-family/">Sony Laptops Are Still Part Of The Sony Family</a></h4>
-
-                                <span className="pfy-post-date"><i className="fal fa-calendar-alt"></i> Jul 07, 2022</span>
-
-                            </div>
-                        </div>
-                        <div className="pfy-post-item pfy-scale pfy-flex">
-                            <div className="pfy-post-thumb pfy-img-commn-style">
-                                <a className="pfy-postthumb" href="#copa-america-suarez-devastated-us/"><img width="1500" height="1000" src="https://interpretasi.id/assets/images/articles/default.png" className="attachment-full size-full wp-post-image" alt="" decoding="async" loading="lazy" srcSet="https://interpretasi.id/assets/images/articles/default.png 1500w, https://interpretasi.id/assets/images/articles/default.png 300w, https://interpretasi.id/assets/images/articles/default.png 1024w, https://interpretasi.id/assets/images/articles/default.png 768w" sizes="(max-width: 1500px) 100vw, 1500px" /></a>
-                            </div>
-                            <div className="pfy-post-content">
-
-                                <a className="benqu-cate-badge" href="#category/sports/" >
-                                    <span>Sports</span>
-                                </a>
-
-                                <h4 className="pfy-post-title"><a href="#copa-america-suarez-devastated-us/">Copa America: Suarez devastated US</a></h4>
-
-                                <span className="pfy-post-date"><i className="fal fa-calendar-alt"></i> Jul 07, 2022</span>
-
-                            </div>
-                        </div>
-                        <div className="pfy-post-item pfy-scale pfy-flex">
-                            <div className="pfy-post-thumb pfy-img-commn-style">
-                                <a className="pfy-postthumb" href="#we-believe-apple-will-announce-iphone/"><img width="1500" height="1000" src="https://interpretasi.id/assets/images/articles/default.png" className="attachment-full size-full wp-post-image" alt="" decoding="async" loading="lazy" srcSet="https://interpretasi.id/assets/images/articles/default.png 1500w, https://interpretasi.id/assets/images/articles/default.png 300w, https://interpretasi.id/assets/images/articles/default.png 1024w, https://interpretasi.id/assets/images/articles/default.png 768w" sizes="(max-width: 1500px) 100vw, 1500px" /></a>
-                            </div>
-                            <div className="pfy-post-content">
-
-                                <a className="benqu-cate-badge" href="#category/tech/" >
-                                    <span>Tech</span>
-                                </a>
-
-                                <h4 className="pfy-post-title"><a href="#we-believe-apple-will-announce-iphone/">We believe Apple Will announce iPhone.</a></h4>
-
-                                <span className="pfy-post-date"><i className="fal fa-calendar-alt"></i> Jul 07, 2022</span>
-
-                            </div>
-                        </div>
-                        <div className="pfy-post-item pfy-scale pfy-flex">
-                            <div className="pfy-post-thumb pfy-img-commn-style">
-                                <a className="pfy-postthumb" href="#10-things-ive-learnt-at-money-freelancer/"><img width="1500" height="1000" src="https://interpretasi.id/assets/images/articles/default.png" className="attachment-full size-full wp-post-image" alt="" decoding="async" loading="lazy" srcSet="https://interpretasi.id/assets/images/articles/default.png 1500w, https://interpretasi.id/assets/images/articles/default.png 300w, https://interpretasi.id/assets/images/articles/default.png 1024w, https://interpretasi.id/assets/images/articles/default.png 768w" sizes="(max-width: 1500px) 100vw, 1500px" /></a>
-                            </div>
-                            <div className="pfy-post-content">
-
-                                <a className="benqu-cate-badge" href="#category/music/" >
-                                    <span>Music</span>
-                                </a>
-
-                                <h4 className="pfy-post-title"><a href="#10-things-ive-learnt-at-money-freelancer/">10 Things I’ve Learnt at Money Freelancer</a></h4>
-
-                                <span className="pfy-post-date"><i className="fal fa-calendar-alt"></i> Jul 07, 2022</span>
-
-                            </div>
-                        </div>
-                    </div>
+                        )}
+                        {/* End Map */}
+                   </div>
                 </div>
             </div>
         </div>
